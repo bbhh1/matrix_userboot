f150926d4450316c627053186da8c56e120a41de
bbhh1 <112596831+bbhh1@users.noreply.github.com>
2022-09-04 15:05:54 +0300

    Update فحص.py

--- jepthon/plugins/فحص.py
+++ jepthon/plugins/فحص.py
@@ -37,7 +37,7 @@ async def amireallyalive(event):
     ms = (end - start).microseconds / 1000
     _, check_sgnirts = check_data_base_heal_th()
     EMOJI = gvarstatus("ALIVE_EMOJI") or "⿻┊‌‎"
-    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**父[ 𝗝𝗘𝗣𝗧𝗛𝗢𝗡 𝗜𝗦 𝗪𝗢𝗥𝗞𝗜𝗡𝗚 ✓ ](t.me/Jepthon)父**"
+    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**父[ 𝗠𝗔𝗧𝗥𝗜𝗫 𝗜𝗦 𝗪𝗢𝗥𝗞𝗜𝗡𝗚 ✓ ](t.me/vv744)父**"
     RR7_IMG = gvarstatus("ALIVE_PIC") or Config.A_PIC
     jepiq_caption = gvarstatus("ALIVE_TEMPLATE") or temp
     caption = jepiq_caption.format(
@@ -72,9 +72,9 @@ async def amireallyalive(event):
 
 
 temp = """{ALIVE_TEXT}
-**‎{EMOJI}‌‎𝙽𝙰𝙼𝙴 𖠄 {mention}** ٫
-**‌‎{EMOJI}‌‎𝙿𝚈𝚃𝙷𝙾𝙽 𖠄 {pyver}** ٫
-**‌‎{EMOJI}‌‎𝙹𝙴𝙿𝚃𝙷𝙾𝙽 𖠄 {telever}** ٫
-**‌‎{EMOJI}‌‎𝚄𝙿𝚃𝙸𝙼𝙴 𖠄 {uptime}** ٫
-‌‎**{EMOJI}‌‎‌‎𝙿𝙸𝙽𝙶 𖠄 {ping}** ٫
-**𖠄 𝗝𝗲𝗽𝘁𝗵𝗼𝗻 𝗨𝘀𝗲𝗿𝗯𝗼𝘁 𖠄**"""
+**‎{EMOJI}‌‎𝑛𝑎𝑚𝑒 𖠄 {mention}** ٫
+**‌‎{EMOJI}‌‎𝒑𝒚𝒕𝒉𝒐𝒏 𖠄 {pyver}** ٫
+**‌‎{EMOJI}‌‎𝑚𝑎𝑡𝑟𝑖𝑥 𖠄 {telever}** ٫
+**‌‎{EMOJI}‌‎𝑢𝑝𝑡𝑖𝑚𝑒 𖠄 {uptime}** ٫
+‌‎**{EMOJI}‌‎‌‎𝑝𝑖𝑛𝑔 𖠄 {ping}** ٫
+**𖠄 𝗺𝗮𝘁𝗿𝗶𝘅 𝗨𝘀𝗲𝗿𝗯𝗼𝘁 𖠄**"""

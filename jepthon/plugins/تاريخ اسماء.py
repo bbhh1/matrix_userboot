0bdb8a40a2c9abb41dd16b2168edeeac596dd694
bbhh1 <112596831+bbhh1@users.noreply.github.com>
2022-09-17 17:04:49 +0300

    Update تاريخ اسماء.py

--- jepthon/plugins/تاريخ اسماء.py
+++ jepthon/plugins/تاريخ اسماء.py
@@ -1,5 +1,5 @@
-# Copyright (C) 2021 JepThon TEAM
-# FILES WRITTEN BY  @lMl10l
+# Copyright (C) 2021 matrix TEAM
+# FILES WRITTEN BY  @Zbbbbb
 import asyncio
 
 from telethon.errors.rpcerrorlist import YouBlockedUserError
@@ -28,12 +28,12 @@ plugin_category = "utils"
         "examples": "{tr}sg @missrose_bot",
     },
 )
-async def _(event):  #   : @lMl10l
+async def _(event):  #   : @Zbbbbb
     "To get name/username history."
     input_str = "".join(event.text.split(maxsplit=1)[1:])
     reply_message = await event.get_reply_message()
     if not input_str and not reply_message:
-        await edit_delete( #ترجمه فريق جـيبثون على التيلكرام
+        await edit_delete( #ترجمه فريق ماتركس على التيلكرام
             event,
             "⌯︙ يـجب الـرد على الشخـص او كتـابة معـرفه مع الأمـر لأظـهار تواريـخ اسمـه",
         )
@@ -67,6 +67,6 @@ async def _(event):  #   : @lMl10l
     for i in check:
         if jasem:
             await event.reply(i, parse_mode=_format.parse_pre)
-        else:#ترجمه فريق جـيبثون على التيلكرام
+        else:#ترجمه فريق ماتركس على التيلكرام
             jasem = True
             await catevent.edit(i, parse_mode=_format.parse_pre)

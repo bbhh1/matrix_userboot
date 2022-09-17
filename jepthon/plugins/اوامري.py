afd29bb53a8e614a3e99fd3acb6cc1fedc8513e6
bbhh1 <112596831+bbhh1@users.noreply.github.com>
2022-09-17 14:37:33 +0300

    Update اوامري.py

--- jepthon/plugins/اوامري.py
+++ jepthon/plugins/اوامري.py
@@ -9,8 +9,8 @@ from jepthon import jepiq
 from ..core import check_owner
 from ..Config import Config
 
-JEP_IC = "https://telegra.ph/file/762989c65df81fc2e96d7.jpg"
-ROE = "**♰ هـذه هي قائمة اوامـر سـورس جيبثون ♰**"
+JEP_IC = "https://telegra.ph/file/26bc0b9ef1b2b5a53d1c5.jpg"
+ROE = "**♰ هـذه هي قائمة اوامـر سـورس ماتركس ♰**"
 
 if Config.TG_BOT_USERNAME is not None and tgbot is not None:
 

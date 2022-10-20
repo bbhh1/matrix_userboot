6f02e8936093397dcbcd7bfc17d1a51ca32e9f51
عبدالعالم <113321419+abdalalaem@users.noreply.github.com>
2022-10-20 12:42:15 +0300

    Update الاوامر.py

--- jepthon/plugins/الاوامر.py
+++ jepthon/plugins/الاوامر.py
@@ -111,7 +111,7 @@ async def _(event):
 async def _(event):
 	if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
 		await event.edit(
-		"** قائمة اوامر الفارات والتخصيص **:\n هـنـا  : \n\n@aaddr2_vars"
+		"** قائمة اوامر الفارات والتخصيص **:\n هـنـا  : \n\SERTHON5 _vars"
 		)
 
 @jepiq.ar_cmd(

5d96be32093f3f59e6974256acce4701bbf9074e
bbhh1 <112596831+bbhh1@users.noreply.github.com>
2022-10-21 14:41:18 +0300

    Update خط غامق.py

--- jepthon/plugins/خط غامق.py
+++ jepthon/plugins/خط غامق.py
@@ -1,4 +1,4 @@
-# DaR
+# DaR - Karar
 # © matrix Team 2023
 # ها شعدك داخل ع الملف تريد تخمط ؟ ابو زربة لهل درجة فاشل  
 from telethon import events
@@ -20,8 +20,26 @@ async def btext(event):
         await edit_delete(event, "**᯽︙ تم اطفاء الخط الغامق بنجاح ✓ **")
         return
 
+@jepiq.on(admin_cmd(pattern="خط رمز"))
+async def btext(event):
+    isramz = gvarstatus("ramz")
+    if not isramz:
+        addgvar ("ramz", "on")
+        await edit_delete(event, "**᯽︙ تم تفعيل الخط الرمز بنجاح ✓**")
+        return
+
+    if isramz:
+        delgvar("ramz")
+        await edit_delete(event, "**᯽︙ تم اطفاء الخط الرمز بنجاح ✓ **")
+        return
+
 @jepiq.on(events.NewMessage(outgoing=True))
 async def reda(event):
     isbold = gvarstatus("bold")
     if isbold:
         await event.edit(f"**{event.message.message}**")
+@jepiq.on(events.NewMessage(outgoing=True))
+async def lMl10l(event):
+    isramz = gvarstatus("ramz")
+    if isramz:
+        await event.edit(f"`{event.message.message}`")

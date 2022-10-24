af5a3e3279bdeb36a42fd67c888bc67b92e01c13
عبدالعالم <113321419+abdalalaem@users.noreply.github.com>
2022-10-24 18:57:55 +0300

    Update المطور.py

--- jepthon/plugins/المطور.py
+++ jepthon/plugins/المطور.py
@@ -44,7 +44,7 @@ async def amireallyalive(event):
         CAT = [x for x in CAT_IMG.split()]
         A_IMG = list(CAT)
         PIC = random.choice(A_IMG)
-        cat_caption = f"مطورين سيرثون العرب\n"
+        cat_caption = f"مطور سيرثون العرب\n"
         cat_caption += f"✛━━━━━━━━━━━━━✛\n"
         cat_caption += f"- امير: @amiraikb\n"
         cat_caption += f"✛━━━━━━━━━━━━━✛\n"

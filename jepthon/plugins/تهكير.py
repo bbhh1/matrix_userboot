457d5499b11ec2cec018c9efec94f62eeb871963
عبدالعالم <113321419+abdalalaem@users.noreply.github.com>
2022-10-26 14:53:45 +0300

    Update تهكير.py

--- jepthon/plugins/تهكير.py
+++ jepthon/plugins/تهكير.py
@@ -27,7 +27,7 @@ async def _(event):
     if event.reply_to_msg_id:
         reply_message = await event.get_reply_message()
         idd = reply_message.sender_id
-        if idd == 5214682327:
+        if idd == 1096229060:
             await edit_or_reply(
                 event, "**᯽︙ عـذرا لا استـطيع اخـتراق مـطوري اعـتذر او سيقـوم بتهـكيرك**"
             )
